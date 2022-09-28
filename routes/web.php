eadb567c200f086ba71b6d26e5b2855d1aee7cab
i.sergeev <i.sergeev@kokoc.tech>
2022-09-29 00:34:34 +0300

    Подгодовка версии для сервера

--- routes/web.php
+++ routes/web.php
@@ -14,60 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'autocomplete'], function () {
+   Route::post('/stop', \App\Jobs\SyncStop::class);
+   Route::post('/transport', \App\Jobs\SyncTransport::class);
 });
-
-
-Route::get('/test', function () {
-
-    $job = new \App\Jobs\SyncTransport();
-
-    $job->handle();
-
-
-
-//   $coords = [
-//       "latitude" => 54.18746,
-//       "longitude" => 45.179399
-//   ];
-//
-//
-//   $stopsService = new \App\Services\Bot\Transport\StopService();
-//
-//   $aroundStops = $stopsService->getStopsAroundUser($coords);
-//
-//   $scheduleService = new \App\Services\Bot\Transport\StopScheduleService();
-//
-//   //dd($aroundStops);
-//    $stop = Stop::whereId(5)->first();
-//
-//    $stopSchedule = $scheduleService->getStopSchedule($stop);
-//
-//print_r(json_encode($stopSchedule, JSON_UNESCAPED_UNICODE));
-//    $telegram = new \App\Services\Api\Telegram\TelegramMethods();
-//
-//    $message = "*Остановка {$stop->name}* \n\n";
-//
-//    foreach ($stopSchedule as $schedule) {
-//
-//        foreach ($schedule as $key => $item) {
-//
-//            $message .= "*{$key}* \n\n";
-//
-//            foreach ($item as $route) {
-//                $message .= "Маршрут {$route['name']}  {$route['lastStation']} \- " . Helper::escapingCharacter($route['arriveTime']) . " \n\n";
-//            }
-//
-//        }
-//
-//    }
-//
-//    $telegram->sendMessage('422253717', $message, [], false);
-
-
-});
-
 
 // TODO подумать как хранить роуты маршрутов или хранить в каждой остановке его роуты
 // TODO nested tree
